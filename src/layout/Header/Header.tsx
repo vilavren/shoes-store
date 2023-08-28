7a4217e9eee1e884572fe0ac1478534a96748a65
vitalii <vilavren@gmail.com>
2023-08-29 00:21:07 +0500

    added: Logo in Header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,13 +1,16 @@
 import cn from 'classnames'
 import React from 'react'
 
+import logoSvg from '../../assets/logo.svg'
+
 import styles from './Header.module.css'
 import { HeaderProps } from './Header.props'
 
 export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
   return (
     <header className={cn(className, styles.header)} {...props}>
-      Header
+      <img className={styles.logo} src={logoSvg} alt="logo" />
+      <h1>Digital Store</h1>
     </header>
   )
 }
