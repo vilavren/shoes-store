88f04e6f560d2a5e472cf54eef26840e7b03b267
vitalii <vilavren@gmail.com>
2023-08-30 00:11:22 +0500

    added: SVG in Component

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,15 +1,14 @@
 import cn from 'classnames'
 import React from 'react'
 
-import logoSvg from '../../assets/logo.svg'
-
 import styles from './Header.module.css'
 import { HeaderProps } from './Header.props'
+import { ReactComponent as Logo } from './logo.svg'
 
 export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
   return (
     <header className={cn(className, styles.header)} {...props}>
-      <img className={styles.logo} src={logoSvg} alt="logo" />
+      <Logo />
       <h1>Digital Store</h1>
     </header>
   )
