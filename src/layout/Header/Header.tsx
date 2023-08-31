e96dc0d9fc22e0c8bb7f57086b3e230bf6f9c5ab
vitalii <vilavren@gmail.com>
2023-08-31 18:17:49 +0500

    added: component Search in Header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,6 +1,8 @@
 import cn from 'classnames'
 import React from 'react'
 
+import { Search } from '../../components/Search/Search'
+
 import styles from './Header.module.css'
 import { HeaderProps } from './Header.props'
 import { ReactComponent as Logo } from './logo.svg'
@@ -8,8 +10,9 @@ import { ReactComponent as Logo } from './logo.svg'
 export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
   return (
     <header className={cn(className, styles.header)} {...props}>
-      <Logo />
-      <h1>Digital Store</h1>
+      <Logo className={styles.logo} />
+      <h1 className={styles.title}>Digital Store</h1>
+      <Search className={styles.search} />
     </header>
   )
 }
