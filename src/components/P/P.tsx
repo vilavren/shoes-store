b5f3c96d44c88ed007d4f41833017c07aeb41151
vitalii <vilavren@gmail.com>
2023-09-01 14:23:28 +0500

    edit: component Button, Htag, P

--- src/components/P/P.tsx
+++ src/components/P/P.tsx
@@ -1,62 +1,29 @@
 import cn from 'classnames'
-import React, { FC } from 'react'
+import React from 'react'
 
 import styles from './P.module.css'
 import { PProps } from './P.props'
 
-export const P: FC<PProps> = ({ size, bold, children }) => {
-  switch (size) {
-    case 'l':
-      return (
-        <p
-          className={cn(styles.l, {
-            [styles.bold]: bold === true,
-          })}
-        >
-          {children}
-        </p>
-      )
-    case 'm':
-      return (
-        <p
-          className={cn(styles.m, {
-            [styles.bold]: bold === true,
-          })}
-        >
-          {children}
-        </p>
-      )
-    case 's':
-      return (
-        <p
-          className={cn(styles.s, {
-            [styles.bold]: bold === true,
-          })}
-        >
-          {children}
-        </p>
-      )
-    case 'xs':
-      return (
-        <p
-          className={cn(styles.xs, {
-            [styles.bold]: bold === true,
-          })}
-        >
-          {children}
-        </p>
-      )
-    case 'tinny':
-      return (
-        <p
-          className={cn(styles.tinny, {
-            [styles.bold]: bold === true,
-          })}
-        >
-          {children}
-        </p>
-      )
-    default:
-      return <></>
-  }
+export const P = ({
+  size,
+  bold,
+  children,
+  className,
+  ...props
+}: PProps): JSX.Element => {
+  return (
+    <p
+      className={cn(className, {
+        [styles.bold]: bold === true,
+        [styles.l]: size === 'l',
+        [styles.m]: size === 'm',
+        [styles.s]: size === 's',
+        [styles.xs]: size === 'xs',
+        [styles.tinny]: size === 'tinny',
+      })}
+      {...props}
+    >
+      {children}
+    </p>
+  )
 }
