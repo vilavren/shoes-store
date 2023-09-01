fc9d7b415afd94bfd28cdbdf7bf563d154450264
vitalii <vilavren@gmail.com>
2023-09-01 13:56:47 +0500

    edit: component Header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,8 +1,11 @@
 import cn from 'classnames'
 import React from 'react'
 
+import { Button } from '../../components/Button/Button'
+import { Appearance } from '../../components/Button/Button.props'
 import { Search } from '../../components/Search/Search'
 
+import { ReactComponent as Cart } from './cart.svg'
 import styles from './Header.module.css'
 import { HeaderProps } from './Header.props'
 import { ReactComponent as Logo } from './logo.svg'
@@ -13,6 +16,13 @@ export const Header = ({ className, ...props }: HeaderProps): JSX.Element => {
       <Logo className={styles.logo} />
       <h1 className={styles.title}>Digital Store</h1>
       <Search className={styles.search} />
+      <a className={styles.link} href="#">
+        Контакты
+      </a>
+      <Button className={styles.button} appearance={Appearance.Primary}>
+        Войти
+      </Button>
+      <Cart />
     </header>
   )
 }
